c0209453424aa1ff3766da2b78506e480d1b802e
Kevin Kelchen <kelchen@cartegraph.com>
2022-02-16 11:05:50 -0600

    Simplify repro to use a single button for triggering an error.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,29 +12,7 @@ export class HomePage {
     private errorService: ErrorService,
   ) {}
 
-  triggerErrorWithGoodBreadcrumb() {
-    this.errorService.addBreadcrumb({
-            category: 'xhr',
-            data: {
-              requestBody: '{ "foo": "bar" }',
-              requestMethod: 'GET',
-              requestURL: 'https://github.com/KevinKelchen/my-fake-request-breadcrumb-url',
-            },
-          });
-
-    this.errorService.captureError(new Error('This error should include a good breadcrumb'));
-  }
-
-  triggerErrorWithBadBreadcrumb() {
-    this.errorService.addBreadcrumb({
-            category: 'xhr',
-            data: {
-              requestBody: '{ "foo": "© bar" }', // 'ü' is another example of a bad character.
-              requestMethod: 'GET',
-              requestURL: 'https://github.com/KevinKelchen/my-fake-request-breadcrumb-url',
-            },
-          });
-
-    this.errorService.captureError(new Error('This error should include a bad breadcrumb'));
+  triggerError() {
+    this.errorService.captureError(new Error('This error should result in unknown metadata'));
   }
 }
