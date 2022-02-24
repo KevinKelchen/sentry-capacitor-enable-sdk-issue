dc201a1cc7dcce2988c2ed117c0cbda9f728e903
Kevin Kelchen <kelchen@cartegraph.com>
2022-02-24 13:05:32 -0600

    Update sample app with reproduction code.

--- src/app/error.service.ts
+++ src/app/error.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { addBreadcrumb, Breadcrumb, captureException, init } from '@sentry/capacitor';
+import { addBreadcrumb, Breadcrumb, captureException, getCurrentHub, init } from '@sentry/capacitor';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,9 @@ export class ErrorService {
       maxBreadcrumbs: 1, // Use 1 breadcrumb so it's easier to debug.
       autoSessionTracking: false, // Don't bother with session tracking.
       debug: true, // Enable debug logging to better understand what's going on with this issue.
+      // enabled: true, // Default value is `true`.
+      // enableNative: true, // Default value is `true`.
+      // enableNativeCrashHandling: true, // Default value is `true`.
     });
   }
 
@@ -24,5 +27,21 @@ export class ErrorService {
 
   captureError(error: any) {
     captureException(error);
+  }
+
+  disable() {
+    // Stop sending events to Sentry.
+    // For more info see: https://github.com/getsentry/sentry-javascript/issues/2039#issuecomment-486674574
+    getCurrentHub().getClient().getOptions().enabled = false;
+
+    // Attempt to stop native events as well.
+    // Casting `Options` as `any` due to the typings not including the additional Capacitor SDK options.
+    // The options are present at runtime if you inspect them.
+    (getCurrentHub().getClient().getOptions() as any).enableNative = false;
+    (getCurrentHub().getClient().getOptions() as any).enableNativeCrashHandling = false;
+  }
+
+  enable() {
+    this.initialize();
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { ErrorService } from '../error.service';
 
 @Component({
@@ -12,7 +13,28 @@ export class HomePage {
     private errorService: ErrorService,
   ) {}
 
-  triggerError() {
-    this.errorService.captureError(new Error('This error should result in unknown metadata'));
+  triggerJavaScriptError() {
+    this.errorService.captureError(new Error('Hardcoded JavaScript error for Sentry Capacitor testing'));
+  }
+
+  async triggerNativeError() {
+     let photo: Photo;
+
+    try {
+      // We've modified `Camera.getPhoto()` via `patch-package` to throw a hardcoded error.
+      photo = await Camera.getPhoto({ resultType: CameraResultType.Uri, source: CameraSource.Camera });
+    } catch (error) {
+      // Don't send an error from the Web layer to Sentry in this case;
+      // we are trying to focus on an error from the native layer.
+      return;
+    }
+  }
+
+  disableErrorService() {
+    this.errorService.disable();
+  }
+
+  enableErrorService() {
+    this.errorService.enable();
   }
 }
